8557ac18346cc5ea7a1882bb902edfd98e642a5b
Nuel <ibk.com@gmail.com>
2022-04-19 00:42:24 +0100

    added some packages to be used in production

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const compression = require('compression');
 require('dotenv').config();
 
 const indexRouter = require('./routes/index');
@@ -26,6 +27,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression()); //Compress all routes
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
